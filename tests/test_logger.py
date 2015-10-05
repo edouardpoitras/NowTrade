2c4d545afef62ff1298c777dc5139b31703fb1f9
Edouard Poitras <edouardpoitras@gmail.com>
2015-10-04 22:02:49 -0400

    logger.py now 100% test coverage
    Signed-off-by: Edouard Poitras <edouardpoitras@gmail.com>

---
+++ tests/test_logger.py
@@ -0,0 +1,14 @@
+import unittest
+import logging
+from nowtrade.logger import Logger, CRITICAL
+
+
+class TestLogger(unittest.TestCase):
+    def test_logger(self):
+        rootLogger = logging.getLogger()
+        logger = Logger(rootLogger.name, logFile='logger.log', consoleLvl=CRITICAL, fileLvl=None)
+        logger.__getstate__()
+        logger.__setstate__({})
+
+if __name__ == "__main__":
+    unittest.main()
