71d385e4e1691f72e5b4c2c39eab431bdb568b63
Edouard Poitras <edouardpoitras@gmail.com>
2015-10-05 19:37:39 -0400

    Cleaned up test_trading_amount.py
    Signed-off-by: Edouard Poitras <edouardpoitras@gmail.com>

--- tests/test_trading_amount.py
+++ tests/test_trading_amount.py
@@ -1,14 +1,23 @@
+"""
+Tests for trading_amount.py module.
+"""
 import unittest
 from nowtrade.trading_amount import NumberOfShares, CapitalPercentage, KellyCriterion
 
 class TestTradingAmount(unittest.TestCase):
-    def test_number_of_shares(self):
-        self.trading_amount = NumberOfShares(1)
-        self.assertEquals(self.trading_amount.get_shares(100, 1000), 1)
-        self.trading_amount = CapitalPercentage(50)
-        self.assertEquals(self.trading_amount.get_shares(100, 1000), 5)
-        self.trading_amount = KellyCriterion(50, 0.5, 0.5)
-        self.assertEquals(self.trading_amount.get_shares(100, 1000), 99000)
+    """
+    Tests trading_amount.py classes.
+    """
+    def test_trading_amount(self):
+        """
+        Simple test for all trading amounts.
+        """
+        trading_amount = NumberOfShares(1)
+        self.assertEquals(trading_amount.get_shares(100, 1000), 1)
+        trading_amount = CapitalPercentage(50)
+        self.assertEquals(trading_amount.get_shares(100, 1000), 5)
+        trading_amount = KellyCriterion(50, 0.5, 0.5)
+        self.assertEquals(trading_amount.get_shares(100, 1000), 99000)
 
 if __name__ == "__main__":
     unittest.main()
