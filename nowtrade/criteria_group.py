f84cab79c47b52b9b5591b4612ca79c9fca3ad10
Edouard Poitras <edouardpoitras@gmail.com>
2015-10-06 22:54:28 -0400

    criteria_group.py now lint free
    Signed-off-by: Edouard Poitras <edouardpoitras@gmail.com>

--- nowtrade/criteria_group.py
+++ nowtrade/criteria_group.py
@@ -1,12 +1,18 @@
-import uuid
-import numpy as np
-import pandas as pd
+"""
+Criteria group module associates various criteria with corresponding actions.
+"""
 from nowtrade.criteria import InMarket, IsLong, IsShort, Not
-from nowtrade.action import Long, LongExit, Short, ShortExit, LONG, LONG_EXIT, SHORT, SHORT_EXIT, NO_ACTION
+from nowtrade.action import Long, LongExit, Short, ShortExit, \
+                            LONG, LONG_EXIT, SHORT, SHORT_EXIT, NO_ACTION
 from nowtrade import logger
 
-class InvalidAction(Exception): pass
-class CriteriaGroup:
+class InvalidAction(Exception):
+    """
+    Exception raised when an invalid action is submitted to a criteria group.
+    """
+    pass
+
+class CriteriaGroup(object):
     """
     For now, since we haven't implemented adding to positions,
     the default is InMarket, NotInMarket, IsLong, IsShort gets
@@ -17,7 +23,7 @@ class CriteriaGroup:
         self.action = action
         self._action = 0
         self.symbol = str(symbol)
-        # Added Actions Criteria Automatically
+        # Add Actions Criteria Automatically
         if self.action == Long():
             self.criteria_list.append(Not(InMarket(self.symbol)))
             self._action = LONG
@@ -32,20 +38,29 @@ class CriteriaGroup:
             self._action = SHORT_EXIT
         else: raise InvalidAction()
         self.logger = logger.Logger(self.__class__.__name__)
-        self.logger.info('New Criteria Group - %s for %s (%s)' %(str(action).upper(), symbol, criteria_list))
+        self.logger.info('New Criteria Group - %s for %s (%s)' \
+                         %(str(action).upper(), symbol, criteria_list))
 
     def __str__(self):
-        return 'CriteriaGroup(criteria_list=%s, action=%s, symbol=%s)' %(self.criteria_list, self.action, self.symbol)
+        return 'CriteriaGroup(criteria_list=%s, action=%s, symbol=%s)' \
+                              %(self.criteria_list, self.action, self.symbol)
     def __repr__(self):
-        return 'CriteriaGroup(criteria_list=%s, action=%s, symbol=%s)' %(self.criteria_list, self.action, self.symbol)
+        return 'CriteriaGroup(criteria_list=%s, action=%s, symbol=%s)' \
+                              %(self.criteria_list, self.action, self.symbol)
 
     def get_result(self, data_frame):
+        """
+        Get the results of this criteria group based on the data_frame
+        provided. If all criteria are True, return the action.
+        """
         results = []
         for criteria in self.criteria_list:
             result = None
-            if criteria.num_bars_required is not None: result = criteria.apply(data_frame[-criteria.num_bars_required:])
+            if criteria.num_bars_required is not None:
+                result = criteria.apply(data_frame[-criteria.num_bars_required:])
             else: result = criteria.apply(data_frame)
             results.append(result)
             self.logger.debug('Criteria - %s: %s' %(criteria, result))
-        if False in results: return NO_ACTION
+        if False in results:
+            return NO_ACTION
         else: return self._action
