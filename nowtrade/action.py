56781ed46ba42ff02e057c0515d6e2199c39f536
Edouard Poitras <edouardpoitras@gmail.com>
2015-10-06 23:20:56 -0400

    trading_profile.py and action.py now lint free
    Signed-off-by: Edouard Poitras <edouardpoitras@gmail.com>

--- nowtrade/action.py
+++ nowtrade/action.py
@@ -1,3 +1,7 @@
+"""
+The actions module contains classes representing all the different actions
+that can be taken in the markets.
+"""
 from nowtrade import logger
 
 LONG = 1
@@ -12,54 +16,88 @@ ACTIONS_MAP = {1: 'LONG',
                -1: 'LONG_EXIT',
                -2: 'SHORT_EXIT'}
 
-class Action:
+class Action(object):
     """
     Abstract Action object represents an action to be taken in the market.
     """
     def __init__(self, name):
         self.name = name.replace(' ', '').replace('_', '').replace('-', '').lower()
         self.logger = logger.Logger(self.__class__.__name__)
-    def __repr__(self): return self.name
-    def __str__(self): return self.name
+    def __repr__(self):
+        return self.name
+    def __str__(self):
+        return self.name
     def __eq__(self, other):
         other = str(other).replace(' ', '').replace('_', '').replace('-', '').lower()
         return other == self.name
-    def raw(self): return NO_ACTION
+    def raw(self):
+        """
+        A raw (int) representation of the action.
+        """
+        return NO_ACTION
 
 class Long(Action):
     """
     Long Action represents going long in the market.
     """
-    def __init__(self): Action.__init__(self, 'long')
-    def raw(self): return LONG
+    def __init__(self):
+        Action.__init__(self, 'long')
+    def raw(self):
+        """
+        A raw (int) representation of the action.
+        """
+        return LONG
 
 class LongExit(Action):
     """
     Long Exit Action represents exiting a long position from the market.
     """
-    def __init__(self): Action.__init__(self, 'longexit')
+    def __init__(self):
+        Action.__init__(self, 'longexit')
     def __eq__(self, other):
         other = str(other).replace(' ', '').replace('_', '').replace('-', '').lower()
         return other == 'longexit' or other == 'exitlong'
-    def raw(self): return LONG_EXIT
+    def raw(self):
+        """
+        A raw (int) representation of the action.
+        """
+        return LONG_EXIT
 
-class ExitLong(LongExit): pass
+class ExitLong(LongExit):
+    """
+    Alias to LongExit().
+    """
+    pass
 
 class Short(Action):
     """
     Short Action represents shorting in the market.
     """
-    def __init__(self): Action.__init__(self, 'short')
-    def raw(self): return SHORT
+    def __init__(self):
+        Action.__init__(self, 'short')
+    def raw(self):
+        """
+        A raw (int) representation of the action.
+        """
+        return SHORT
 
 class ShortExit(Action):
     """
     Short Exit Action represents exiting a short position from the market.
     """
-    def __init__(self): Action.__init__(self, 'shortexit')
+    def __init__(self):
+        Action.__init__(self, 'shortexit')
     def __eq__(self, other):
         other = str(other).replace(' ', '').replace('_', '').replace('-', '').lower()
         return other == 'shortexit' or other == 'exitshort'
-    def raw(self): return SHORT_EXIT
+    def raw(self):
+        """
+        A raw (int) representation of the action.
+        """
+        return SHORT_EXIT
 
-class ExitShort(ShortExit): pass
+class ExitShort(ShortExit):
+    """
+    Alias to ShortExit().
+    """
+    pass
--- nowtrade/trading_profile.py
+++ nowtrade/trading_profile.py
@@ -1,7 +1,15 @@
-from nowtrade import logger
-from nowtrade.report import Report
+"""
+Trading profile module used to emulate a user's trading resources and
+constraints in the markets.
 
-class TradingProfile:
+"""
+from nowtrade import logger
+
+class TradingProfile(object):
+    """
+    Simple trading profile object that holds information pertraining to a
+    user's trading resources and constraints in the market.
+    """
     def __init__(self, capital, trading_amount, trading_fee, slippage=0.0):
         self.capital = capital
         self.trading_amount = trading_amount
@@ -9,5 +17,21 @@ class TradingProfile:
         self.slippage = slippage
         self.logger = logger.Logger(self.__class__.__name__)
         self.logger.info('Initialized - %s' %self)
-    def __str__(self): return 'TradingProfile(capital=%s, trading_amount=%s, trading_fee=%s, slippage=%s' %(self.capital, self.trading_amount, self.trading_fee, self.slippage)
-    def __repr__(self): return 'TradingProfile(capital=%s, trading_amount=%s, trading_fee=%s, slippage=%s' %(self.capital, self.trading_amount, self.trading_fee, self.slippage)
+    def __str__(self):
+        return 'TradingProfile(capital=%s, \
+                               trading_amount=%s, \
+                               trading_fee=%s, \
+                               slippage=%s' \
+                              %(self.capital, \
+                                self.trading_amount, \
+                                self.trading_fee, \
+                                self.slippage)
+    def __repr__(self):
+        return 'TradingProfile(capital=%s, \
+                               trading_amount=%s, \
+                               trading_fee=%s, \
+                               slippage=%s' \
+                              %(self.capital, \
+                                self.trading_amount, \
+                                self.trading_fee, \
+                                self.slippage)
