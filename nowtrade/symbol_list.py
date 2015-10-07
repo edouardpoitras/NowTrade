837feddb0695f5b7ff53bfeee81fdbead6bf73a7
Edouard Poitras <edouardpoitras@gmail.com>
2015-10-06 23:56:45 -0400

    symbol_list.py now lint free
    Signed-off-by: Edouard Poitras <edouardpoitras@gmail.com>

--- nowtrade/symbol_list.py
+++ nowtrade/symbol_list.py
@@ -1,24 +1,40 @@
+"""
+This module is used as a way of managing and accessing the different symbols
+being used in a strategy.
+"""
 from nowtrade import logger
 
 class SymbolList(object):
+    """
+    Holds a list of symbol objects and makes it easy for the user to
+    retrieve them by name.
+    """
     def __init__(self, symbols):
         self.symbols = [Symbol(symbol) for symbol in symbols]
         self.logger = logger.Logger(self.__class__.__name__)
         self.logger.info(symbols)
     def get(self, symbol):
+        """
+        Returns the symbol object named after the symbol parameter.
+        """
         symbol = symbol.upper()
-        for s in self.symbols:
-            if symbol == s.symbol: return s
+        for sym in self.symbols:
+            if symbol == sym.symbol:
+                return sym
         return None
-    def __repr__(self): return str(self.symbols)
-    def __str__(self): return str(self.symbols)
+    def __repr__(self):
+        return str(self.symbols)
+    def __str__(self):
+        return str(self.symbols)
     def __iter__(self):
         return iter(self.symbols)
-class StockList(SymbolList):
-    def __init__(self, symbols):
-        SymbolList.__init__(self, symbols)
+StockList = SymbolList
 
 class Symbol(object):
+    """
+    A symbol object allows for easily accessing a symbol's various attributes
+    such as the OHLCV and Adj.C of a symbol.
+    """
     def __init__(self, symbol):
         self.symbol = symbol.upper()
         self.symbol = self.symbol
@@ -28,7 +44,11 @@ class Symbol(object):
         self.close = '%s_Close' %self.symbol
         self.volume = '%s_Volume' %self.symbol
         self.adj_close = '%s_Adj Close' %self.symbol
-    def __repr__(self): return str(self.symbol)
-    def __str__(self): return str(self.symbol)
-    def __hash__(self): return hash(self.symbol)
-    def __eq__(self, other): return self.symbol == other
+    def __repr__(self):
+        return str(self.symbol)
+    def __str__(self):
+        return str(self.symbol)
+    def __hash__(self):
+        return hash(self.symbol)
+    def __eq__(self, other):
+        return self.symbol == other
