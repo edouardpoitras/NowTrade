62c0c0e53a4ca6acf8b2b607c131a8d8259f7f67
Edouard Poitras <edouardpoitras@gmail.com>
2015-10-07 21:03:37 -0400

    Fixed a small bug in data_connection.md example
    Signed-off-by: Edouard Poitras <edouardpoitras@gmail.com>

--- scripts/update_oanda_currency.py
+++ scripts/update_oanda_currency.py
@@ -3,7 +3,7 @@
 from nowtrade import data_connection
 ACCOUNT_ID = ''
 ACCESS_TOKEN = ''
-data_connection.populate_oanda_currency(ACCOUNT_ID, ACCESS_TOKEN, ['EUR_USD', 'USD_JPY', 'GBP_USD', 'USD_CAD', 'USD_CHF', 'AUD_USD'], granularity='M5', periods=5000, db='oanda-5min')
-data_connection.populate_oanda_currency(ACCOUNT_ID, ACCESS_TOKEN, ['EUR_USD', 'USD_JPY', 'GBP_USD', 'USD_CAD', 'USD_CHF', 'AUD_USD'], granularity='M15', periods=5000, db='oanda-15min')
-data_connection.populate_oanda_currency(ACCOUNT_ID, ACCESS_TOKEN, ['EUR_USD', 'USD_JPY', 'GBP_USD', 'USD_CAD', 'USD_CHF', 'AUD_USD'], granularity='H1', periods=5000, db='oanda-1hour')
-data_connection.populate_oanda_currency(ACCOUNT_ID, ACCESS_TOKEN, ['EUR_USD', 'USD_JPY', 'GBP_USD', 'USD_CAD', 'USD_CHF', 'AUD_USD'], granularity='H4', periods=5000, db='oanda-4hour')
+data_connection.populate_oanda_currency(ACCOUNT_ID, ACCESS_TOKEN, ['EUR_USD', 'USD_JPY', 'GBP_USD', 'USD_CAD', 'USD_CHF', 'AUD_USD'], granularity='M5', periods=5000, database='oanda-5min')
+data_connection.populate_oanda_currency(ACCOUNT_ID, ACCESS_TOKEN, ['EUR_USD', 'USD_JPY', 'GBP_USD', 'USD_CAD', 'USD_CHF', 'AUD_USD'], granularity='M15', periods=5000, database='oanda-15min')
+data_connection.populate_oanda_currency(ACCOUNT_ID, ACCESS_TOKEN, ['EUR_USD', 'USD_JPY', 'GBP_USD', 'USD_CAD', 'USD_CHF', 'AUD_USD'], granularity='H1', periods=5000, database='oanda-1hour')
+data_connection.populate_oanda_currency(ACCOUNT_ID, ACCESS_TOKEN, ['EUR_USD', 'USD_JPY', 'GBP_USD', 'USD_CAD', 'USD_CHF', 'AUD_USD'], granularity='D', periods=5000, database='oanda-day')
