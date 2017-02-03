5b96720aaa9f96cd39d3b4ffa747e049319e59b8
Edouard Poitras <epoitras@linuxfoundation.org>
2017-02-03 00:15:32 -0500

    Adding mysql-python to conda in build
    Signed-off-by: Edouard Poitras <epoitras@linuxfoundation.org>

--- nowtrade/report.py
+++ nowtrade/report.py
@@ -426,4 +426,3 @@ def _exiting_ongoing_trade(data_frame, trade, action):
     data_frame['PL_%s' %trade.symbol][-1] = profit_loss - fee
     data_frame['CHANGE_VALUE_%s' %trade.symbol][-1] = enter_change
     data_frame['CHANGE_PERCENT_%s' %trade.symbol][-1] = percent_change
-
